7e564db83b975710beac7875771aa708b984d1ee
Thibauld Favre <tfavre@gmail.com>
2016-12-22 00:22:36 -0500

    * ajout script d'envoi d'emails par batch

---
+++ send_email/send-batch-emails.rb
@@ -0,0 +1,105 @@
+require '../config/keys.local.rb'
+require 'mandrill'
+require 'pg'
+require 'csv'
+
+DEBUG=false
+TEST_EMAIL=false
+PGPWD=DEBUG ? PGPWD_TEST : PGPWD_LIVE
+PGNAME=DEBUG ? PGNAME_TEST : PGNAME_LIVE
+PGUSER=DEBUG ? PGUSER_TEST : PGUSER_LIVE
+PGHOST=DEBUG ? PGHOST_TEST : PGHOST_LIVE
+
+
+def generate_code(size = 6)
+	charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
+	(0...size).map{ charset.to_a[rand(charset.size)] }.join
+end
+
+mandrill=Mandrill::API.new(MANDRILLKEY)
+db=PG.connect(
+	:dbname=>PGNAME,
+	"user"=>PGUSER,
+	"password"=>PGPWD,
+	"host"=>PGHOST,
+	"port"=>PGPORT
+)
+
+if TEST_EMAIL then
+	puts "TEST_EMAIL"
+end
+
+def tag_voters(db,mandrill)
+	if TEST_EMAIL then
+		users=[{'email'=>'tfavre@gmail.com','user_key'=>'XXXX'}]
+	else
+		q="UPDATE users as u SET tags=array_append(tags,'voteur2') FROM ballots as b WHERE b.email=u.email AND NOT (u.tags @> ARRAY['voteur2']) AND b.vote_id=1 AND b.vote_status='success' RETURNING u.email, u.user_key"
+		res=db.exec(q)
+		users=res.num_tuples.zero? ? nil : res
+	end
+	send_emails(mandrill,users,'laprimaire-org-vote-merci-avec-don',"Votre vote a bien été enregistré !") unless users.nil?
+	return users
+end
+
+def tag_donors(db,mandrill)
+	if TEST_EMAIL then
+		users=[{'email'=>'tfavre@gmail.com','user_key'=>'XXXX','amount'=>100.00}]
+	else
+		q="UPDATE users as u SET tags=array_append(tags,'donateur_parti') FROM donations as d WHERE d.email=u.email AND NOT (u.tags @> ARRAY['donateur_parti']) AND d.origin='payzen' RETURNING u.email, u.user_key,d.amount"
+		res=db.exec(q)
+		users=res.num_tuples.zero? ? nil : res
+	end
+	send_emails(mandrill,users,"laprimaire-org-don-thank-you","Merci énormément pour votre don !") unless users.nil?
+	return users
+end
+
+def remind_new_signups_to_auth(db,mandrill)
+	if TEST_EMAIL then
+		users=[{'email'=>'tfavre@gmail.com','user_key'=>'XXXX'}]
+	else
+		q="SELECT u.email,u.user_key,u.registered FROM users AS u WHERE u.registered > current_date -2 AND u.registered < current_date AND u.email NOT IN (SELECT email FROM ballots WHERE vote_id=1) AND u.email_status>=0"
+		res=db.exec(q)
+		users=res.num_tuples.zero? ? nil : res
+	end
+	send_emails(mandrill,users,"laprimaire-org-forgot-to-auth-after-signup","Un petit oubli ?") unless users.nil?
+	return users
+end
+
+def send_emails(mandrill,users,template,subject)
+	return if (users.nil? || template.nil? || subject.nil?)
+	emails=[]
+	users.each do |r|
+		message= {
+			:subject=>subject,
+			:to=>[{
+				:email=> "#{r['email']}"
+			}],
+			:merge_vars=>[{
+				:rcpt=>"#{r['email']}",
+				:vars=>[ 
+					{:name=>"EMAIL",:content=>"#{r['email']}"},
+					{:name=>"USER_KEY",:content=>"#{r['user_key']}"},
+					{:name=>"AMOUNT",:content=>"#{r['amount']}"}
+				]
+			}]
+		}
+		emails.push(message)
+	end
+	results=[]
+	emails.each do |k|
+		begin
+			result=mandrill.messages.send_template(template,[],k)
+			puts "sending email to #{k[:to][0][:email]} #{result.inspect}"
+			results.push({:email=>result[0]['email'], :status=>result[0]['status'],:reject_reason=>result[0]['reject_reason'],:id=>result[0]['_id']})
+			sleep(1)
+		rescue Mandrill::Error => e
+			msg="A mandrill error occurred: #{e.class} - #{e.message}"
+			puts msg
+		end
+	end
+	File.write(Time.new.strftime("%Y-%m%d-%H:%M")+template+".txt",JSON.dump(results))
+end
+
+tag_voters(db,mandrill)
+tag_donors(db,mandrill)
+remind_new_signups_to_auth(db,mandrill)
--- send_email/send_email.rb
+++ send_email/send_email.rb
@@ -1,4 +1,4 @@
-require '../../config/keys.local.rb'
+require '../config/keys.local.rb'
 require 'mandrill'
 require 'pg'
 require 'csv'
@@ -378,8 +378,6 @@ def email_citoyens_toutes_candidates(db,mandrill)
 	File.write("20160609_email_shoot.txt",JSON.dump(results))
 end
 
-select u.email, u.firstname, u.lastname from users as u inner join supporters as s on (s.email=u.email) inner join 
-
 def email_recap_soutiens(db,mandrill)
 	#get_citizens="SELECT candidate_id,candidate_key,name,email FROM candidates WHERE email IS NOT NULL AND email='tfavre@gmail.com' LIMIT 2"
 	#get_candidates="SELECT candidate_id,candidate_key,name,email FROM candidates WHERE email IS NOT NULL AND verified AND vision is NULL"
@@ -420,7 +418,242 @@ def email_recap_soutiens(db,mandrill)
 	File.write("20160609_email_shoot.txt",JSON.dump(results))
 end
 
-email_recap_soutiens(db,mandrill)
+def email_citizens_no_access(db,mandrill)
+	#get_citizens="SELECT user_key,email,firstname,lastname FROM users WHERE email IS NOT NULL AND email='tfavre@gmail.com' LIMIT 2"
+	get_citizens="select email,firstname,lastname,user_key from users as u where tags @> ARRAY['voter'] AND email_status=2 AND email not in (select distinct email from auth_history)"
+	#get_candidates="SELECT candidate_id,candidate_key,name,email FROM candidates WHERE email IS NOT NULL AND verified AND vision is NULL"
+	#get_citizens="select c.email from citizens as c where c.email NOT IN (select ci.email from citizens as ci inner join mongo_supporteurs as su on (su.email=ci.email));"
+	#get_citizens="select u.email, u.firstname, u.lastname from users as u where telegram_id is not null;"
+	res_citizens=db.exec(get_citizens)
+	if not res_citizens.num_tuples.zero? then
+		emails=[]
+		res_citizens.each do |r|
+			message= {
+				:to=>[{
+					:email=> "#{r['email']}"
+				}],
+				:merge_vars=>[{
+					:rcpt=>"#{r['email']}",
+					:vars=>[ 
+						{:name=>"EMAIL",:content=>"#{r['email']}"},
+						{:name=>"LNAME",:content=>"#{r['lastname']}"},
+						{:name=>"USER_KEY",:content=>"#{r['user_key']}"},
+						{:name=>"FNAME",:content=>"#{r['firstame']}"}
+					]
+				}]
+			}
+			emails.push(message)
+		end
+	end
+	results=[]
+	emails.each do |k|
+		begin
+			result=mandrill.messages.send_template("laprimaire-org-citoyens-no-access",[],k)
+			puts "sending email to #{k[:to][0][:email]} #{result.inspect}"
+			results.push({:email=>result[0]['email'], :status=>result[0]['status'],:reject_reason=>result[0]['reject_reason'],:id=>result[0]['_id']})
+			sleep(1/4)
+		rescue Mandrill::Error => e
+			msg="A mandrill error occurred: #{e.class} - #{e.message}"
+			puts msg
+		end
+	end
+	File.write("20161106_email_shoot_citizens_no_access.txt",JSON.dump(results))
+end
+
+def email_citizens_vote_issue(db,mandrill)
+	#get_citizens="SELECT user_key,email,firstname,lastname FROM users WHERE email IS NOT NULL AND email='tfavre@gmail.com' LIMIT 2"
+	get_citizens="select distinct u.email,u.user_key from ballots as b inner join candidates_ballots as cb on (cb.ballot_id=b.ballot_id) inner join users as u on (u.email=b.email) where cb.vote_status is not null AND cb.vote_status!='complete' AND cb.date_notified<'2016-11-06' AND cb.date_notified>'2016-10-26' order by u.email asc"
+	res_citizens=db.exec(get_citizens)
+	if not res_citizens.num_tuples.zero? then
+		emails=[]
+		res_citizens.each do |r|
+			message= {
+				:to=>[{
+					:email=> "#{r['email']}"
+				}],
+				:merge_vars=>[{
+					:rcpt=>"#{r['email']}",
+					:vars=>[ 
+						{:name=>"EMAIL",:content=>"#{r['email']}"},
+						{:name=>"USER_KEY",:content=>"#{r['user_key']}"}
+					]
+				}]
+			}
+			emails.push(message)
+		end
+	end
+	results=[]
+	emails.each do |k|
+		begin
+			result=mandrill.messages.send_template("laprimaire-org-erreur-when-voting",[],k)
+			puts "sending email to #{k[:to][0][:email]} #{result.inspect}"
+			results.push({:email=>result[0]['email'], :status=>result[0]['status'],:reject_reason=>result[0]['reject_reason'],:id=>result[0]['_id']})
+			sleep(1/4)
+		rescue Mandrill::Error => e
+			msg="A mandrill error occurred: #{e.class} - #{e.message}"
+			puts msg
+		end
+	end
+	File.write("20161106_email_shoot_citizens_vote_issue.txt",JSON.dump(results))
+end
+
+def email_citizens_missing_votes(db,mandrill)
+	#get_citizens="SELECT user_key,email,firstname,lastname, 3 as missing FROM users WHERE email IS NOT NULL AND email='tfavre@gmail.com' LIMIT 2"
+	get_citizens="select y.email,y.missing,u.user_key from (select z.email,5-array_length(z.votes,1) as missing from (select b.email,array_agg(cb.vote_status) as votes from candidates_ballots as cb inner join ballots as b on (b.ballot_id=cb.ballot_id) where cb.vote_status='complete' and cb.date_notified<'2016-11-06' group by b.email) as z) as y inner join users as u on (u.email=y.email) where y.missing>0 and u.email_status=2 order by y.email asc"
+	res_citizens=db.exec(get_citizens)
+	if not res_citizens.num_tuples.zero? then
+		emails=[]
+		res_citizens.each do |r|
+			message= {
+				:to=>[{
+					:email=> "#{r['email']}"
+				}],
+				:merge_vars=>[{
+					:rcpt=>"#{r['email']}",
+					:vars=>[ 
+						{:name=>"EMAIL",:content=>"#{r['email']}"},
+						{:name=>"NB_CANDIDATS",:content=>"#{r['missing']}"},
+						{:name=>"USER_KEY",:content=>"#{r['user_key']}"}
+					]
+				}]
+			}
+			emails.push(message)
+		end
+	end
+	results=[]
+	emails.each do |k|
+		begin
+			result=mandrill.messages.send_template("laprimaire-org-citoyens-x-votes-missing",[],k)
+			puts "sending email to #{k[:to][0][:email]} #{result.inspect}"
+			results.push({:email=>result[0]['email'], :status=>result[0]['status'],:reject_reason=>result[0]['reject_reason'],:id=>result[0]['_id']})
+			sleep(1/4)
+		rescue Mandrill::Error => e
+			msg="A mandrill error occurred: #{e.class} - #{e.message}"
+			puts msg
+		end
+	end
+	File.write("20161106_email_shoot_citizens_missing_votes.txt",JSON.dump(results))
+end
+
+def email_citizens_ballot_without_vote(db,mandrill)
+	#get_citizens="SELECT user_key,email,firstname,lastname, 3 as missing FROM users WHERE email IS NOT NULL AND email='tfavre@gmail.com' LIMIT 2"
+	get_citizens="select z.email,u.user_key from (select b.email,array_agg(cb.vote_status) FILTER (WHERE vote_status is not null) as votes from candidates_ballots as cb inner join ballots as b on (b.ballot_id=cb.ballot_id) group by b.email) as z inner join users as u on (u.email=z.email) where z.votes is null and u.email_status=2 order by z.email asc"
+	res_citizens=db.exec(get_citizens)
+	if not res_citizens.num_tuples.zero? then
+		emails=[]
+		res_citizens.each do |r|
+			message= {
+				:to=>[{
+					:email=> "#{r['email']}"
+				}],
+				:merge_vars=>[{
+					:rcpt=>"#{r['email']}",
+					:vars=>[ 
+						{:name=>"EMAIL",:content=>"#{r['email']}"},
+						{:name=>"USER_KEY",:content=>"#{r['user_key']}"}
+					]
+				}]
+			}
+			emails.push(message)
+		end
+	end
+	results=[]
+	emails.each do |k|
+		begin
+			result=mandrill.messages.send_template("laprimaire-org-ballot-without-vote",[],k)
+			puts "sending email to #{k[:to][0][:email]} #{result.inspect}"
+			results.push({:email=>result[0]['email'], :status=>result[0]['status'],:reject_reason=>result[0]['reject_reason'],:id=>result[0]['_id']})
+			sleep(0.8)
+		rescue Mandrill::Error => e
+			msg="A mandrill error occurred: #{e.class} - #{e.message}"
+			puts msg
+		end
+	end
+	File.write("20161106_email_shoot_citizens_ballot_wo_vote.txt",JSON.dump(results))
+end
+
+def email_citizens_pre_authenticate(db,mandrill)
+	#get_citizens="SELECT user_key,email,firstname,lastname, 3 as missing FROM users WHERE email IS NOT NULL AND email='tfavre@gmail.com' LIMIT 2"
+	#get_citizens="select email,user_key from users where validation_level<3 and email_status=2 and (registered>'2016-11-07' and registered<'2016-12-12') order by registered asc"
+	get_citizens="select email,user_key from users where validation_level<3 and email_status=2 and (registered<'2016-11-07' and registered>'2016-07-01') order by registered asc"
+	res_citizens=db.exec(get_citizens)
+	if not res_citizens.num_tuples.zero? then
+		emails=[]
+		res_citizens.each do |r|
+			message= {
+				:to=>[{
+					:email=> "#{r['email']}"
+				}],
+				:merge_vars=>[{
+					:rcpt=>"#{r['email']}",
+					:vars=>[ 
+						{:name=>"EMAIL",:content=>"#{r['email']}"},
+						{:name=>"USER_KEY",:content=>"#{r['user_key']}"}
+					]
+				}]
+			}
+			emails.push(message)
+		end
+	end
+	results=[]
+	emails.each do |k|
+		begin
+			result=mandrill.messages.send_template("laprimaire-org-pre-auth-2nd-tour-de-vote",[],k)
+			puts "sending email to #{k[:to][0][:email]} #{result.inspect}"
+			results.push({:email=>result[0]['email'], :status=>result[0]['status'],:reject_reason=>result[0]['reject_reason'],:id=>result[0]['_id']})
+			sleep(1)
+		rescue Mandrill::Error => e
+			msg="A mandrill error occurred: #{e.class} - #{e.message}"
+			puts msg
+		end
+	end
+	File.write("20161212_email_shoot_citizens_pre_auth_2.txt",JSON.dump(results))
+end
+
+def email_citizens_authenticated_2nd_vote(db,mandrill)
+	#get_citizens="SELECT user_key,email,firstname,lastname, 3 as missing FROM users WHERE email IS NOT NULL AND email='tfavre@gmail.com' LIMIT 2"
+	#get_citizens="select email,user_key from users where validation_level<3 and email_status=2 and (registered>'2016-11-07' and registered<'2016-12-12') order by registered asc"
+	get_citizens="select email,user_key from tmp_emails order by email asc"
+	res_citizens=db.exec(get_citizens)
+	if not res_citizens.num_tuples.zero? then
+		emails=[]
+		res_citizens.each do |r|
+			message= {
+				:to=>[{
+					:email=> "#{r['email']}"
+				}],
+				:merge_vars=>[{
+					:rcpt=>"#{r['email']}",
+					:vars=>[ 
+						{:name=>"EMAIL",:content=>"#{r['email']}"},
+						{:name=>"USER_KEY",:content=>"#{r['user_key']}"}
+					]
+				}]
+			}
+			emails.push(message)
+		end
+	end
+	results=[]
+	emails.each do |k|
+		begin
+			result=mandrill.messages.send_template("laprimaire-org-2eme-tour-de-vote",[],k)
+			puts "sending email to #{k[:to][0][:email]} #{result.inspect}"
+			results.push({:email=>result[0]['email'], :status=>result[0]['status'],:reject_reason=>result[0]['reject_reason'],:id=>result[0]['_id']})
+			sleep(2)
+		rescue Mandrill::Error => e
+			msg="A mandrill error occurred: #{e.class} - #{e.message}"
+			puts msg
+		end
+	end
+	File.write("20161217_email_shoot_citizens_authenticated_2nd_vote.txt",JSON.dump(results))
+end
+
+email_citizens_authenticated_2nd_vote(db,mandrill)
+#email_citizens_pre_authenticate(db,mandrill) #DONE
+#email_citizens_missing_votes(db,mandrill) #DONE
+#email_citizens_ballot_without_vote(db,mandrill) #DONE
+#email_citizens_vote_issue(db,mandrill)
+#email_citizens_no_access(db,mandrill)
 #email_citoyens_toutes_candidates(db,mandrill)
 #email_citoyens_appel_aux_maires(db,mandrill)
 #email_candidat_urgent(db,mandrill)
